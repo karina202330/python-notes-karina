912fa0c0304d9df200e85e883269da7838c8f42d
karina202330 <karinakesur0730@gmail.com>
2025-07-05 14:34:42 +0530

    Create 07_file_handling.py

---
+++ 07_file_handling.py
@@ -0,0 +1,10 @@
+# 07_file_handling.py
+# File Handling
+
+# File handling using 'with' (auto close)
+# with open("filename.txt", "r") as file:
+#     content = file.read()
+#     print(content)
+
+# Modes:
+# "r" – read, "w" – write, "a" – append, "x" – create
